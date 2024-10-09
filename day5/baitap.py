44adfea2d865647acc3bab5832e8f68fdbc24024
Nam Nguyen <namnv12bg@gmail.com>
2024-10-10 06:38:11 +0700

    done

--- day5/baitap.py
+++ day5/baitap.py
@@ -11,3 +11,8 @@ new_string2=friends.copy()
 print(new_string)
 print(new_string2)
 print(friends[1:-1])
+student=[["SV001","Bob",23],["SV002", "Kenny",34], ["SV003", "Henry", 45]]
+print(f"Thông tin của sinh viên thứ nhất là ID = {student[0][0]} Name= {student[0][1]} Age = {student[0][2]}")
+print(student[1][2])
+print(student[1:])
+print(student[2][0])
