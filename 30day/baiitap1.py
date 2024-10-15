baea16fae19efb57cc1dc9288ffd8a3bea5729c7
Nam Nguyen <namnv12bg@gmail.com>
2024-10-15 23:17:52 +0700

    m

---
+++ 30day/baiitap1.py
@@ -0,0 +1,23 @@
+a=3
+b=4
+print(a+b)
+print(a-b)
+print(3*4)
+print(3%4)
+print(3/4)
+print(3//4)
+print(a**b)
+inter=5
+roarr=5.6
+complex=5+3j
+lst=["con gà",1,"quả na",4,2.5]
+boolen=True
+str="con gà"
+print(lst[0])
+Tup=(1,2,4,6,4,5)
+print(Tup)
+print(type(Tup))
+set1={1,2,3,3,5}
+print(set1)
+dict={"Tên":"Bob", "tuổi":34,"Lớp":"A001"}
+print(type(dict))
--- day7/motsoham.py
+++ day7/motsoham.py
@@ -1,3 +1,12 @@
-list=[[1,2,3,4],[5,7,9]]
-total=sum(list,[0])
-print(total)
+# list=[[1,2,3,4],[5,7,9]]
+# print(list[0][0])
+# print(list[0])
+# print(max(list[0]))
+#hàm join nối được 1 list thành 1 string
+# lst=[1,3,5,7,9]
+# s="".join(map(str,lst)) #hàn map giúp convert từng phần tử của list thành chuỗi
+# print(s)
+# int(s)
+# print(s)
+lst=[8,1,2,3]
+print(sum(lst,10))
