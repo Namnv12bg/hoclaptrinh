26e620ff41e718119dc98d2e621be71c93efb892
Nam Nguyen <namnv12bg@gmail.com>
2024-10-23 06:46:27 +0700

    b9

---
+++ day8/baitap.py
@@ -0,0 +1,47 @@
+#Bài tập 1
+# in_num = int(input("Mời bạn nhập số nguyên "))
+# s="It's even"
+# if in_num % 2 != 0:
+#     s="It's odd "
+#     print(s)
+#Bài tập 2
+#Cách 1
+# year = int(input("Mời bạn nhập vào số năm "))
+# if year % 400==0:
+#     print("Năm nhuận")
+# elif year %100 == 0:
+#     print("Năm không nhuận")
+# elif year %4==0:
+#     print("Năm nhuận")
+# else:
+#     print("Năm không nhuận")
+#Cách 2
+# year=int(input("Mời bạn nhập số năm "))
+# if year %400 ==0:
+#     print("năm nhuận")
+# elif year %100 ==0:
+#     print("năm không nhuận")
+# elif year%4==0:
+#     print("năm nhuận")
+# else:
+#     print("năm không nhuận")
+#Bài tập 3
+# tup1 = map(float,input("Mời bạn nhập các số cần so sánh, mỗi số cách nhau bằng đấu tab ").split("/t"))
+# print(f"số lớn nhất là {max(tup1)}") 
+# print(f"số nhỏ nhất là {min(tup1)}")
+# tup1 = tuple(map(float, input("Mời bạn nhập các số cần so sánh, mỗi số cách nhau bằng dấu tab: ").split("\t")))
+# print(f"Số lớn nhất là {max(tup1)}") 
+# print(f"Số nhỏ nhất là {min(tup1)}")
+# year=int(input("Mời bạn nhập số năm "))
+# if year %4 !=0 or (year % 100==0 and year % 400 !=0):
+#     print("Năm không nhuận")
+# else:
+#     print("Năm nhuận")
+# for _ in range(5):
+#     print("Hello world!")
+# for i in range(1,23):
+    # if i %2==0:
+        # print(i, end=" ")
+s = input()
+while s != "p":
+    print("a")
